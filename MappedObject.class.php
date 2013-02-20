2d0f68f0a873f79b9bbc7aacc6c8c41b4f2e693f
Alan Rager <rager@laptop>
2013-02-19 16:47:22 -0800

    removed proprietary stuff and bettered abstraction

--- MappedObject.class.php
+++ MappedObject.class.php
@@ -1,5 +1,4 @@
 <?php
-namespace basedPHP;
 /**	MappedObject.class.php
  * 	class		MappedObject
  * 	author		Alan Rager
@@ -11,81 +10,22 @@ namespace basedPHP;
 
 abstract class MappedObject implements Iterator {
 
-	protected $fields = array();
-	protected $required_fields = array();
 	protected $data;
 	protected $updates = array();
+	protected $fields = array();
+	private $keys = array();
 
 	public function __construct(Array $fields = array()) {
-		$this->verifyInput($fields);
 		$this->data = $fields;
+		$this->fields = array_keys($fields);
 	}
 
-	protected function verifyFields($data, $input = true) {
-		$out = array();
-
-		foreach($data as $field=>$val) {
-
-			if(!isset($this->fields[$field])) {
-				continue;
-			}
-
-			switch($this->fields[$field]) {
-				case 'int':
-					$out[$field] = intval($val);
-					break;
-				case 'string':
-					$out[$field] = strval($val);
-					break;
-				case 'float':
-					$out[$field] = floatval($val);
-					break;
-				case 'bool':
-					$out[$field] = (bool) $val;
-					break;
-				case 'time':
-					if($input) {
-						$out[$field] = new DateTime($val);
-					} else {
-						$datefield = $val;
-						$out[$field] = $val->format('c');
-					}
-					break;
-			}
-		}
-
-		return $out;
-	}
-
-	protected function verifyInput($data) {
-		return $this->verifyFields($data, true);
-	}
-
-	protected function verifyOutput($data) {
-		return $this->verifyFields($data, false);
-	}
 
 	//Magic setter - is called when you try to set object properties
 	public function __set($field, $val) {
-		if(array_key_exists($field, $this->fields)) {
-			//A little finagling to make dealing with time more simple
-			if($this->fields[$field] === 'time') {
-				if(is_int($val) || is_numeric($val)) {
-					//Turn timestamps into DateTime
-					$this->updates[$field] = new DateTime("@$val");
-				} else if($val instanceof DateTime) {
-					//Just assign DateTime objects straight off
-					$this->updates[$field] = $val;
-				} else {
-					//Turn date strings into DateTime
-					$this->updates[$field] = new DateTime($val);
-				}
-			} else {
-				//Default behavior - just assign to the update array
-				$this->updates[$field] = $val;
-			}
-		} else {
-			throw new InvalidFieldException(get_called_class(), $field);
+		if(!isset($this->data[$field]) || $this->data[$field] !== $val) {
+			$this->updates[$field] = $val;
+			$this->fields = array_keys($this->toArray());
 		}
 	}
 
@@ -102,60 +42,51 @@ abstract class MappedObject implements Iterator {
 		}
 
 		//Finally, return a default value of NULL
-		if(array_key_exists($field, $this->fields)) {
-			return NULL;
-		}
-
-		throw new InvalidFieldException(get_called_class(), $field);
+		return NULL;
 	}
 
 	//Magic to check of a property is set
 	public function __isset($field) {
-		//Check to see if $field is set and if it is in $this->fields
-		return (array_key_exists($field, $this->updates) && $this->updates[$field] !== NULL) ||
-			(array_key_exists($field, $this->data) && $this->data[$field] !== NULL);
+		return array_key_exists($field, $this->updates) ||
+			array_key_exists($field, $this->data);
 	}
 
 	//Magic to unset object properties
 	public function __unset($field) {
-		if(!array_key_exists($field, $this->fields)) {
-			throw new InvalidFieldException(get_called_class(), $field);
+		if(array_key_exists($field, $this->updates)) {
+			unset($this->updates[$field]);
+		} else if(array_key_exists($field, $this->data)) {
+			$this->__set($field, NULL);
 		}
-
-		$this->__set($field, NULL);
+		$this->keys = array_keys($this->toArray());
 	}
 
 	//Batch set fields
 	public function importData(array $data) {
 		$this->updates = array_merge(
 			$this->updates,
-			$this->verifyInput($data)
+			$data
 		);
+		$this->keys = array_keys($this->toArray());
 		return $this;
 	}
 
 	//Create an array out of the object
 	public function toArray() {
-		return $this->verifyOutput(
-			array_merge($this->data, $this->updates)
-		);
+		return array_merge($this->data, $this->updates);
 	}
 
 	public function getUpdatedFields() {
-		return $this->verifyOutput($this->updates);
+		return $this->updates;
 	}
 
 	//These allow subscribing to certain events in a Model's lifetime
 	public function __beforeSave() {
-		foreach($this->required_fields as $req) {
-			if(!isset($this->{$req})) {
-				throw new MissingFieldException(get_called_class(), $req);
-			}
-		}
 	}
 
 	public function __afterSave() {
 		$this->data = array_merge($this->data, $this->updates);
+		$this->updates = array();
 	}
 
 	public function __beforeTrash() {
@@ -166,20 +97,25 @@ abstract class MappedObject implements Iterator {
 
 	//Iterator methods - gotta have these to iterate
 
+	private $iterationpointer = 0;
+
 	public function current() {
 		return $this->{$this->key()};
 	}
 
 	public function rewind() {
-		reset($this->fields);
+		//reset($this->fields);
+		$this->iterationpointer = 0;
 	}
 
 	public function key() {
-		return key($this->fields);
+		return isset($this->fields[$this->iterationpointer]) ? 
+			$this->fields[$this->iterationpointer]:NULL;
 	}
 
 	public function next() {
-		next($this->fields);
+		//next($this->fields);
+		$this->iterationpointer++;
 	}
 
 	public function valid() {
--- PDOMapper.class.php
+++ PDOMapper.class.php
@@ -1,5 +1,4 @@
 <?php
-namespace basedPHP;
 /**	PDOMapper.class.php
  * 	class		PDOMapper
  * 	author		Alan Rager
@@ -11,11 +10,16 @@ namespace basedPHP;
  * 	all necessary queries.
  */
 
-require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'DBExceptions.php');
+//Because I can't really trust the servers to have a timezone set...
+date_default_timezone_set('UTC');
+require_once(dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'include_core.php');
+core_class('db/DBExceptions.php');
 
 abstract class PDOMapper {
 
 	protected $db;
+	protected $fields = array();
+	protected $required_fields = array();
 
 	public function __construct(PDO $db) {
 		$this->db = $db;
@@ -25,35 +29,230 @@ abstract class PDOMapper {
 	abstract protected function getTableName();
 
 	//Utility function to perform all the things
-	protected function executeQuery($sql, $args) {
-		$query = $this->db->prepare($sql);
-		$query->execute($args);
+	protected function executeQuery($sql, $args = array()) {
+		//Don't prepare unless args are given
+		if(empty($args)) {
+			return $this->db->query($sql);
+		}
 
-		return $query;
+		try {
+			$query = $this->db->prepare($sql);
+			$query->execute($args);
+
+			return $query;
+		} catch (PDOException $e) {
+			error_log("Erroneous SQL: [$sql]");
+			error_log("SQL args: ".json_encode($args));
+			throw $e;
+		}
+	}
+	
+	public function getFields()
+	{
+		return $this->fields; 
+	}
+
+	//Utility function to transform DateTime into the fromat MySQL wants
+	protected function dateToString(DateTime $d) {
+		return $d->format('Y-m-d H:i:s');
+	}
+
+	protected function getKey() {
+		$flip = array_flip($this->fields);
+		return isset($flip['key']) ? $flip['key'] : 'id';
+	}
+
+	protected function hasRequiredFields($ob) {
+		//Make sure that the object has all the fields that are named in required_fields
+		$missing = array_diff($this->required_fields,
+			array_keys($ob->toArray())
+		);
+
+		if(count($missing) > 0) {
+			throw new MissingFieldException($this->getModelName(),
+				implode(', ', $missing)
+			);
+		}
+		return true;
+	}
+
+	protected function verifyField($field, $val) {
+		if(!isset($this->fields[$field])) {
+			throw new InvalidFieldException($this->getModelName(), $field);
+		}
+
+		switch($this->fields[$field]) {
+			case 'key':
+			case 'int':
+				return intval($val);
+			case 'string':
+				return strval($val);
+			case 'float':
+				return floatval($val);
+			case 'bool':
+				return (bool) $val;
+			case 'time':
+				if(is_int($val) || is_numeric($val)) {
+					return new DateTime("@$val");
+				}
+				if(!$val instanceof DateTime) {
+					return new DateTime($val);
+				}
+				return $val;
+		}
+	}
+
+	protected function verifyFields($data) {
+		$out = array();
+
+		foreach($data as $field=>$val) {
+			if(!isset($this->fields[$field])) {
+				continue;
+			}
+			$out[$field] = $this->verifyField($field, $val);
+		}
+
+		return $out;
 	}
 
 	//CRUD
 
+	public function tableSql() {
+		$sql = 'CREATE TABLE `'.$this->getTableName().'` (';
+
+		$field_sql = array();
+		$constraint_sql = array();
+
+		foreach($this->fields as $field=>$type) {
+			switch($type) {
+				case 'key':
+					$row = "`$field` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT";
+					$constraint_sql[] = "PRIMARY KEY (`$field`)";
+					break;
+				case 'int':
+					$row = "`$field` INT(10)";
+					break;
+				case 'string':
+					$row = "`$field` VARCHAR(255)";
+					break;
+				case 'bool':
+					$row = "`$field` TINYINT(1)";
+					break;
+				case 'time':
+					$row = "`$field` DATETIME";
+					break;
+			}
+			if(in_array($field, $this->required_fields)) {
+				$row .= ' NOT NULL';
+			} else {
+				$row .= ' DEFAULT NULL';
+			}
+			$field_sql[] = $row;
+		}
+
+		$sql .= implode(',', $field_sql);
+
+		if(!empty($constraint_sql)) {
+			$sql .= ', '.implode(',', $constraint_sql);
+		}
+		$sql .= ');';
+		return $sql;
+	}
+
+	public function saveBatch(array $obs) {
+		if(empty($obs)) {
+			return 0;
+		}
+
+		$newobs = array();
+		$table = $this->getTableName();
+		$model = $this->getModelName();
+
+		$sql = "REPLACE INTO `$table` ";
+		$fields = false;
+
+		$value_fillers = array();
+		$args = array();
+
+		foreach($obs as $ob) {
+			try {
+				$this->hasRequiredFields($ob);
+			} catch(MissingFieldException $e) {
+				//Maybe throw exception?
+				error_log($e->getMessage());
+			}
+
+			if(!($ob instanceof $model)) {
+				throw new MapperException("Cannot save object of type $classname with the $model_name mapper");
+			}
+			if($ob->{$this->getKey()} > 0) {
+				$this->save($ob);
+			} else {
+				if(!$fields) {
+					//Add the fields to the insert query
+					$fields = array();
+					foreach($ob as $field=>$val) {
+						if($field !== $this->getKey()) {
+							$fields[] = $field;
+						}
+					}
+					$sql .= '(`'.implode('`,`',$fields).'`) VALUES ';
+				}
+
+				$value_fillers[] = '('.implode(',',array_fill(0,count($fields), '?')).') ';
+
+				foreach($fields as $field) {
+					$args[] = $ob->{$field};
+				}
+			}
+		}
+
+		$sql .= implode(',', $value_fillers);
+
+		$q = $this->executeQuery($sql, $args);
+		return $q->rowCount();
+	}
+
 	public function save(MappedObject $ob) {
 		$ob->__beforeSave();
+		$this->hasRequiredFields($ob);
 		$model_name = $this->getModelName();
-		if(!is_object($ob) || !($ob instanceof $model_name)) {
+		if(!($ob instanceof $model_name)) {
 			$classname = get_class($ob);
 			throw new MapperException("Cannot save object of type $classname with the $model_name mapper");
 		}
 
 		$table = $this->getTableName();
-		$fields = $ob->getUpdatedFields();
+
+		$id = $ob->{$this->getKey()};
+
+		if($id > 0) {
+			$fields = $ob->getUpdatedFields();
+		} else {
+			$fields = $ob->toArray();
+		}
+
+		if(count($fields) === 0) {
+			return $ob->{$this->getKey()};
+		}
 
 		//Don't ever let the id field be "set"
-		if(array_key_exists('id', $fields)) {
-			unset($fields['id']);
+		if(array_key_exists($this->getKey(), $fields)) {
+			unset($fields[$this->getKey()]);
 		}
 
 		$fieldnames = array_keys($fields);
 
+		//Process things that can't go into the database as-is
+		foreach($fieldnames as $field) {
+			//Format DateTime objects to fit what MySQL expects
+			if($fields[$field] instanceof DateTime) {
+				$fields[$field] = $this->dateToString($fields[$field]);
+			}
+		}
+
 		//If it's an object that has already been saved, id > 0
-		if($ob->id > 0) {
+		if($ob->{$this->getKey()} > 0) {
 			$sql = "UPDATE `$table` SET ";
 
 			$args = array();
@@ -64,9 +263,9 @@ abstract class PDOMapper {
 				$args[":$key"] = $val;
 			}
 
-			$args[':id'] = $ob->id;
+			$args[':id'] = $ob->{$this->getKey()};
 
-			$sql .= implode(',', $set_strings)." WHERE id=:id";
+			$sql .= implode(',', $set_strings).' WHERE `'.$this->getKey().'`=:id';
 		} else {
 			$sql = "INSERT INTO `$table` ";
 
@@ -84,38 +283,42 @@ abstract class PDOMapper {
 		}
 
 		$this->executeQuery($sql, $args);
-		$ob->id = $this->db->lastInsertId();
+		$insertid = $this->db->lastInsertId();
+		if($insertid > 0) {
+			$ob->{$this->getKey()} = $insertid;
+		}
 
 		$ob->__afterSave();
-		return $ob->id;
+		return $ob->{$this->getKey()};
 	}
 
 	public function trash(MappedObject $ob) {
 		$ob->__beforeTrash();
-		$query = $this->executeQuery('DELETE FROM '.$this->getTableName().' WHERE id=:id',
+		$query = $this->executeQuery('DELETE FROM '.$this->getTableName().' WHERE `'.$this->getKey().'`=:id',
 			array(
-				':id' => $ob->id
+				':id' => $ob->{$this->getKey()}
 			)
 		);
 		$ob->__afterTrash();
-		$ob->id = -1;
+		$ob->{$this->getKey()} = -1;
 		return $query->rowCount() > 0;
 	}
 
-	protected function queryObjects($sql, $args) {
+	protected function queryObjects($sql, array $args = array()) {
 		$st = $this->executeQuery($sql, $args);
 
 		$out = array();
 		$model = $this->getModelName();
 
 		while($next = $st->fetch(PDO::FETCH_ASSOC)) {
+			$next = $this->verifyFields($next);
 			$out[$next['id']] = new $model($next);
 		}
 
 		return $out;
 	}
 
-	protected function queryObject($sql, $args) {
+	protected function queryObject($sql, array $args = array()) {
 		$st = $this->executeQuery($sql, $args);
 
 		$out = array();
@@ -123,6 +326,7 @@ abstract class PDOMapper {
 
 		$data = $st->fetch(PDO::FETCH_ASSOC);
 		if(is_array($data)) {
+			$data = $this->verifyFields($data);
 			return new $model($data);
 		}
 		return NULL;
@@ -130,7 +334,7 @@ abstract class PDOMapper {
 
 	public function fetch($id) {
 		return $this->queryObject(
-			'SELECT * FROM '.$this->getTableName().' WHERE id=:id',
+			'SELECT * FROM `'.$this->getTableName().'` WHERE `'.$this->getKey().'`=:id',
 			array(':id' => $id)
 		);
 	}
@@ -142,10 +346,28 @@ abstract class PDOMapper {
 		}
 
 		return $this->queryObjects(
-			'SELECT * FROM '.$this->getTableName().' WHERE id IN('.
+			'SELECT * FROM `'.$this->getTableName().'` WHERE `'.$this->getKey().'` IN('.
 				implode(',', array_fill(0, count($ids), '?')).')',
 			array_values($ids)
 		);
 	}
+
+        public function fetchPage($page_num = 0, $page_size = 50) {
+                $query = $this->db->prepare(
+                        'SELECT * FROM `'.$this->getTableName().'` LIMIT :off,:rows'
+                );
+                $query->bindValue(':off', intval($page_num * $page_size));
+                $query->bindValue(':rows', intval($page_size));
+                $query->execute();
+
+                $out = array();
+		$model = $this->getModelName();
+
+                while($next = $query->fetch(PDO::FETCH_ASSOC)) {
+                        $out[] = new $model($next);
+                }
+
+                return $out;
+        }
 }
 
